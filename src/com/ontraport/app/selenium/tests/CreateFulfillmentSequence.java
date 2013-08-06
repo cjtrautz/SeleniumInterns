a5cc3209ba5395ec7ea5b167c747161067dac890
Nicholas Cross <nickdaquick@nickdaquick-ThinkPad-W530.(none)>
2013-08-05 17:28:37 -0700

    new tests #pull

--- src/com/ontraport/app/selenium/tests/CreateFulfillmentSequence.java
+++ src/com/ontraport/app/selenium/tests/CreateFulfillmentSequence.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.ontraport.app.selenium.tools.OntraportFirefoxTest;
 
@@ -23,6 +25,8 @@ public class CreateFulfillmentSequence extends OntraportFirefoxTest{
 			driver = new FirefoxDriver();
 			e.printStackTrace();
 		}
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
 		long varTimeStamp = Calendar.getInstance().getTimeInMillis();
 		String Search =  String.valueOf(varTimeStamp);
 		String seqName = "test" + varTimeStamp;
@@ -32,6 +36,8 @@ public class CreateFulfillmentSequence extends OntraportFirefoxTest{
         driver.findElement(By.xpath("//div[@id='ontraport_panel_action_new']//span[.='New Sequence']")).click();
         driver.findElement(By.xpath("//div[@class='ussr-component-section-components']//button[normalize-space(.)='Create']")).click();
         
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='ussr-chrome-panel-pane']/div[3]/div/div[3]/div/div/div/input")));
+
         driver.findElement(By.xpath("//div[@id='ussr-chrome-panel-pane']/div[3]/div/div[3]/div/div/div/input")).click();
         driver.findElement(By.xpath("//div[@id='ussr-chrome-panel-pane']/div[3]/div/div[3]/div/div/div/input")).clear();
         driver.findElement(By.xpath("//div[@id='ussr-chrome-panel-pane']/div[3]/div/div[3]/div/div/div/input")).sendKeys(seqName);
@@ -43,11 +49,12 @@ public class CreateFulfillmentSequence extends OntraportFirefoxTest{
         
         driver.findElement(By.xpath("//div[@id='ussr-chrome-panel-pane']//button[normalize-space(.)='Save']")).click();
         
-        driver.findElement(By.xpath("//div[@id='ussr-chrome-panel-pane']/div[1]")).click();
+        driver.findElement(By.xpath("//div[2]/div/section/div/div/div[1]/div[5]/div/div/div/input")).click();
 		driver.findElement(By.xpath("//input[@type='search']")).clear();
 		driver.findElement(By.xpath("//input[@type='search']")).sendKeys(Search);
 		driver.findElement(By.xpath("//span[@class='ussr-icon ussr-icon-search']")).click();
         
+        assertTrue(appUtilities.isElementPresent(driver, By.xpath("//tbody[@class='ussr-component-collection-body']//a[normalize-space(.)='" + "Step" + "']")));
         assertTrue(appUtilities.isElementPresent(driver, By.xpath("//tbody[@class='ussr-component-collection-body']//a[normalize-space(.)='" + seqName + "']")));
         driver.findElement(By.xpath("//tbody[@class='ussr-component-collection-body']//a[normalize-space(.)='" + seqName + "']")).click();
         
