77083e1f6457a79bc835b3763919a5cd2ef6d5c8
Nicholas Cross <nickdaquick@nickdaquick-ThinkPad-W530.(none)>
2013-08-07 09:23:10 -0700

    working

--- src/com/ontraport/app/selenium/tests/CreateRule.java
+++ src/com/ontraport/app/selenium/tests/CreateRule.java
@@ -2,9 +2,13 @@ package com.ontraport.app.selenium.tests;
 
 import java.util.Calendar;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.AssertJUnit;
 
 import com.ontraport.app.selenium.tools.OntraportFirefoxTest;
 
@@ -14,54 +18,76 @@ public class CreateRule extends OntraportFirefoxTest {
 
 	@Test
 	public void testCreateRule() throws Exception {
-		driver.get(baseUrl + "/");
+
+		//driver.get(baseUrl + "/");
 		//login
-
-		appUtilities.loginToApp(driver, "tester", "passphrases are easy to break");
-		String ruleName = "SelRule"+Calendar.getInstance().getTimeInMillis();
-
-		System.out.println ("sdahfkljhsdaf:"+ruleName);
+		//appUtilities.loginToApp(driver, "tester", "passphrases are easy to break");
+		WebDriver driver;
+		try {
+			driver = getDriver();
+		} catch (Exception e) {
+			System.out.println("get Driver failed");
+			driver = new FirefoxDriver();
+			e.printStackTrace();
+		}
+		long varTimeStamp = Calendar.getInstance().getTimeInMillis();
+		String ruleName = "SelRule" + varTimeStamp;
+		String Search = String.valueOf(varTimeStamp);
+		driver.findElement(By.xpath("//aside[@id='ussr-chrome-sidebar']//span[.='Contacts']")).click();
 
 		//Click Rules
 		driver.findElement(By.xpath("//*[@class='primary-nav-sub-item']/a//span[text()='Rules']")).click();
 		//Click on New Rule
 		driver.findElement(By.xpath("//*[@id='panelbuttonbar']/div//span[text()='New Rule']")).click();
 		//Type the Rule Name
-		driver.findElement(By.xpath("//input[@type='text']")).clear();
-		driver.findElement(By.xpath("//input[@type='text']")).sendKeys(ruleName);
+		driver.findElement(By.xpath("//div[@id='ussr-chrome-panel-pane']/div[3]/div/div[3]/div/div/div/input")).click();
+        driver.findElement(By.xpath("//div[@id='ussr-chrome-panel-pane']/div[3]/div/div[3]/div/div/div/input")).clear();
+        driver.findElement(By.xpath("//div[@id='ussr-chrome-panel-pane']/div[3]/div/div[3]/div/div/div/input")).sendKeys(ruleName);
 
-		appUtilities.selectRuleDropDown (driver, "WHEN THIS HAPPENS:","Select Trigger...", "A Certain field is updated");
-		Thread.sleep(5000);
-		appUtilities.selectRuleDropDown(driver, "WHEN THIS HAPPENS:", "Select Field...", "City");
+        selectRuleDropDown (driver, "WHEN THIS HAPPENS:","Select Trigger...", "A Certain field is updated");
+        selectRuleDropDown(driver, "WHEN THIS HAPPENS:", "Select Field...", "City");
 
-		appUtilities.selectRuleDropDown(driver, "IF THIS IS TRUE:", "Select Condition (optional)...", "Field is this value");
-		Thread.sleep(3000);
-		appUtilities.selectRuleDropDown(driver, "IF THIS IS TRUE:", "Select Field...", "First Name");
+        selectRuleDropDown(driver, "IF THIS IS TRUE:", "Select Condition (optional)...", "Field is this value");
+        selectRuleDropDown(driver, "IF THIS IS TRUE:", "Select Field...", "First Name");
 
-		appUtilities.selectRuleDropDown(driver, "IF THIS IS TRUE:", "Select Condition...", "Equal To");
-
-
-		driver.findElement(By.xpath("//input[@placeholder='Enter Value...']")).clear();
-		driver.findElement(By.xpath("//input[@placeholder='Enter Value...']")).sendKeys("selenium");
+        selectRuleDropDown(driver, "IF THIS IS TRUE:", "Select Condition...", "Equal To");
 
 
-		appUtilities.selectRuleDropDown(driver, "THEN DO THIS:", "Select Action...", "Change the value of a field");
-		appUtilities.selectRuleDropDown(driver, "THEN DO THIS:", "Select Field...", "Last Name");
+        driver.findElement(By.xpath("//input[@placeholder='Enter Value...']")).clear();
+        driver.findElement(By.xpath("//input[@placeholder='Enter Value...']")).sendKeys("selenium");
 
-		driver.findElement(By.xpath("(//input[@type='text'])[9]")).clear();
-		driver.findElement(By.xpath("(//input[@type='text'])[9]")).sendKeys("selenium");
+        driver.findElement(By.xpath("//div[@class='ussr-component-helper-field-display-rule-editor']/div[3]/div[2]/div/div[1]/div/div[2]/div[2]/div[2]/div/div/input")).click();
+        driver.findElement(By.xpath("//div[@class='ussr-component-helper-field-display-rule-editor']/div[3]/div[2]/div/div[1]/div/div[2]/div[2]/div[2]/div/div/input")).clear();
+        driver.findElement(By.xpath("//div[@class='ussr-component-helper-field-display-rule-editor']/div[3]/div[2]/div/div[1]/div/div[2]/div[2]/div[2]/div/div/input")).sendKeys("change");
+        driver.findElement(By.xpath("//ul[@class='ussr-component-drilldownselect-ul']//div[.='Change the value of a field']")).click();
+        selectRuleDropDown(driver, "THEN DO THIS:", "Select Field...", "Last Name");
 
-		Thread.sleep(3000);		
+        driver.findElement(By.xpath("//div[@class='ussr-component-helper-field-display-rule-editor']/div[3]/div[2]/div/div[1]/div/div[2]/div[2]/div[1]/div/table/tbody/tr/td[4]/span[1]/div/div/input")).click();
+        driver.findElement(By.xpath("//div[@class='ussr-component-helper-field-display-rule-editor']/div[3]/div[2]/div/div[1]/div/div[2]/div[2]/div[1]/div/table/tbody/tr/td[4]/span[1]/div/div/input")).clear();
+        driver.findElement(By.xpath("//div[@class='ussr-component-helper-field-display-rule-editor']/div[3]/div[2]/div/div[1]/div/div[2]/div[2]/div[1]/div/table/tbody/tr/td[4]/span[1]/div/div/input")).sendKeys("selenium");
 
-		driver.findElement(By.xpath("//button//span[text()='Save']")).click();
-		Thread.sleep(3000);
+        driver.findElement(By.xpath("//button//span[text()='Save']")).click();
 
-		Assert.assertTrue(appUtilities.isElementPresent(driver, By.xpath("//a[normalize-space(text())='" + ruleName +"']")));
-		//Logout
-		appUtilities.logOutOfApp(driver);
+        driver.findElement(By.xpath("//div[@id='ussr-chrome-panel-pane']/div[1]")).click();
+        driver.findElement(By.xpath("//input[@type='search']")).clear();
+        driver.findElement(By.xpath("//input[@type='search']")).sendKeys(Search);
+        driver.findElement(By.xpath("//span[@class='ussr-icon ussr-icon-search']")).click();
+        AssertJUnit.assertTrue(appUtilities.isElementPresent(driver, By.xpath("//a[normalize-space(text())='" + ruleName +"']")));
+        driver.findElement(By.xpath("//aside[@id='ussr-chrome-sidebar']//span[.='Contacts']")).click();
+
+        //Logout
+        //appUtilities.logOutOfApp(driver);
+}
+
+
+	public void selectRuleDropDown (WebDriver driver, String ruleDesc, String placeHolder, String option) throws InterruptedException{
+		driver.findElement(By.xpath("//div[div[text()='"+ruleDesc+"']]//div[input[normalize-space(@placeholder)='"+ placeHolder +"']]/descendant::button")).click();
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[div[text()='"+ruleDesc+"']]//div[input[normalize-space(@placeholder)='"+placeHolder+"']]/descendant::li/div[normalize-space(text())='"+option+"']")));
+		driver.findElement(By.xpath("//div[div[text()='"+ruleDesc+"']]//div[input[normalize-space(@placeholder)='"+placeHolder+"']]/descendant::li/div[normalize-space(text())='"+option+"']")).click();
+
 	}
 
-
-
-
 }
+
+
