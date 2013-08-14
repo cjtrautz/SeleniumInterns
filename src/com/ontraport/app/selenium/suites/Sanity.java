958d14b2630fa9b5e1ebbe13a5c20c6fec4ca7ff
Bill Brown <bill@ontraport.com>
2013-08-14 13:48:09 -0700

    Testing the ability to create videos by test.

--- src/com/ontraport/app/selenium/suites/Sanity.java
+++ src/com/ontraport/app/selenium/suites/Sanity.java
@@ -116,22 +116,29 @@ public class Sanity {
 	@BeforeClass
 	public static void setUp() throws Exception {
 		driver = new FirefoxDriver();
+		Sanity.
 		baseUrl = "http://ontraport.com.dev";
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		driver.manage().window().setPosition(new Point(0,0));
 		driver.manage().window().setSize(new Dimension(1600, 1200));
-		proc = Runtime.getRuntime()
-		.exec("ffmpeg -r 30 -s 1600x1200 -f x11grab -i :1.0 -vcodec msmpeg4v2 -qscale 2 ./report/selenium/" + (Sanity.class.getSimpleName()) + ".avi");
 	}
 
 	@AfterClass
 	public static void tearDown() throws Exception {
 		driver.quit();
-		proc.destroy();
 		String verificationErrorString = verificationErrors.toString();
 		if (!"".equals(verificationErrorString)) {
 		fail(verificationErrorString);
 		}
+	}
+	@BeforeClass
+	public void startVideo() throws Exception {
+		proc = Runtime.getRuntime()
+		.exec("ffmpeg -r 30 -s 1600x1200 -f x11grab -i :1.0 -vcodec msmpeg4v2 -qscale 2 ./report/selenium/" + (getClass().getSimpleName()) + ".avi");
+	}
+	@AfterClass
+	public void stopVideo() throws Exception {
+		proc.destroy();
 	}
 	public static WebDriver getDriver(){
 		return driver;
