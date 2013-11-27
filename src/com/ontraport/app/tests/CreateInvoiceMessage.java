d6448eaf805c4a485aa861cc3fa75d74194c3215
Nicholas Cross <nickdaquick@nickdaquick-ThinkPad-W530.(none)>
2013-11-26 17:52:20 -0800

    here it is

--- src/com/ontraport/app/tests/CreateInvoiceMessage.java
+++ src/com/ontraport/app/tests/CreateInvoiceMessage.java
@@ -15,54 +15,10 @@ import com.ontraport.app.tools.AbstractTest;
 public class CreateInvoiceMessage extends AbstractTest
 {
     @Test
-    public void testCreateDoubleOptinMessage ()
+    public void testCreateInvoiceMessage ()
     {
         Contact_ListAll contactListAll = (Contact_ListAll) new Contact_ListAll().init();
-        contactListAll.menuUser.open();
-        Account_View account_View = contactListAll.menuUser.clickAdmin();
-        MessageTemplate_ListAll messageTemplate_ListAll = account_View.clickDoubleOptInOrInvoiceManager();
-        MessageTemplate_TypeSelection messageTemplate_TypeSelection = messageTemplate_ListAll.clickNewMessageTemplate();
-        MessageTemplate_CreateType1 messageTemplate_CreateType1 = messageTemplate_TypeSelection.clickCreateDoubleOptIn();
-        messageTemplate_CreateType1.enterMessageName("SelDoubleOptInMessage");
-        messageTemplate_CreateType1.enterSendOutName("Selenium");
-        messageTemplate_CreateType1.enterReplyToEmail("selenium@email.com");
-        messageTemplate_CreateType1.openMailFromPane();
-        messageTemplate_CreateType1.selectMailFrom(1);
-        messageTemplate_CreateType1.enterSubject("SelSubject");
-        messageTemplate_CreateType1.enterMessageBody("SelBody");
-        messageTemplate_CreateType1.openMergeFieldPane();
-        messageTemplate_CreateType1.selectMergeField("First Name");
-        messageTemplate_CreateType1.enterMessagePlainTextBody("SelBody");
-        messageTemplate_ListAll = messageTemplate_CreateType1.clickSave();
-        messageTemplate_ListAll.formSearch.find("SelDoubleOptInMessage");
-        
-        //verify that it exists
-        if(messageTemplate_ListAll.verifyMessage("SelDoubleOptInMessage")==null)
-        {
-            fail("couldn't find created message");
-        }
 
-        MessageTemplate_Edit messageTemplate_Edit = messageTemplate_ListAll.clickMessage("SelDoubleOptInMessage");
-        if(messageTemplate_Edit.verifyBody("SelBody[First Name]")==null)
-        {
-            fail("couldn't find message body");
-        }
-        if(messageTemplate_Edit.verifyReplyTo("selenium@email.com")==null)
-        {
-            fail("couldn't find reply to");
-        }
-        if(messageTemplate_Edit.verifySendName("Selenium")==null)
-        {
-            fail("couldn't find send name");
-        }
-        if(messageTemplate_Edit.verifySubject("SelSubject")==null)
-        {
-            fail("couldn't find subject");
-        }
-        if(messageTemplate_Edit.verifyPlainTextEmail("SelBody")==null)
-        {
-            fail("couldn't find plain text email");
-        }
 
     }
 }
