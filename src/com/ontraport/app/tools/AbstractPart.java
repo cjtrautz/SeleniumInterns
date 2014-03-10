4c9e6f217a2934c11ace9e56409b46c5ef043dad
Steve Layman <slayman@ontraport>
2014-03-10 13:42:14 -0700

    movetabtooverflow done

--- src/com/ontraport/app/tools/AbstractPart.java
+++ src/com/ontraport/app/tools/AbstractPart.java
@@ -144,4 +144,20 @@ public abstract class AbstractPart
     {
         AbstractPart.trys = trys;
     }
+    public static String toTitleCase(String string) {
+        final String ACTIONABLE_DELIMITERS = " '-/"; // these cause the character following
+        // to be capitalized
+
+        StringBuilder sb = new StringBuilder();
+        boolean capNext = true;
+        
+        for (char c : string.toCharArray()) {
+            c = (capNext)
+            ? Character.toUpperCase(c)
+            : Character.toLowerCase(c);
+            sb.append(c);
+            capNext = (ACTIONABLE_DELIMITERS.indexOf((int) c) >= 0); // explicit cast not needed
+        }
+        return sb.toString();
+    }
 }
