c8cf377484c03d2edf7b8a0405d137a393025e59
Nick Cross <nick@ontraport.com>
2014-03-12 17:24:15 -0700

    testing production

---
+++ src/com/ontraport/app/tests/DeleteSMSMessageFromSequence.java
@@ -0,0 +1,30 @@
+package com.ontraport.app.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.ontraport.app.pages.Contact_ListAll;
+import com.ontraport.app.pages.Message_ListAll;
+import com.ontraport.app.tools.AbstractTest;
+
+public class DeleteSMSMessageFromSequence extends AbstractTest
+{
+    @Test
+    public void testDeleteSMSMessage ()
+    {
+        Contact_ListAll contactListAll = (Contact_ListAll) new Contact_ListAll().init();
+        Message_ListAll message_ListAll = contactListAll.menuPrimary.clickMessageListAll();
+        message_ListAll.formSearch.find(value.get("Messages", "sms_from_sequence"));
+        message_ListAll.selectAllOnPage();
+        message_ListAll.drawerActions.clickDeleteMessage();
+        message_ListAll.dialogBox.clickOk();
+        
+        //verify
+        if(message_ListAll.verifyNoMessage()==null)
+        {
+            fail("found deleted message");
+        }
+        
+    }
+}
