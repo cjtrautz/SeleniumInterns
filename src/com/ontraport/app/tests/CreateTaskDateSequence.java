f339e80b5bd9da210d545362b24ab25546d48912
Nick Cross <nick@ontraport.com>
2014-03-17 17:35:20 -0700

    contact owner changes

--- src/com/ontraport/app/tests/CreateTaskDateSequence.java
+++ src/com/ontraport/app/tests/CreateTaskDateSequence.java
@@ -56,7 +56,7 @@ public class CreateTaskDateSequence extends AbstractTest
         {
             fail("couldn't find sequence task due date");
         }
-        if(sequence_Edit.verifyAssignee("Contact owner")==null)
+        if(sequence_Edit.verifyAssignee("Contact Owner")==null)
         {
             fail("couldn't find sequence task assignee");
         }
