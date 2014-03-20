53dd607691bb2de521b67ed7947b0c7f25c413db
Nick Cross <nick@ontraport.com>
2014-03-19 17:16:17 -0700

    removed unnecessary files

--- src/com/ontraport/app/parts/bb/MenuHelp.java
+++
@@ -1,9 +0,0 @@
-package com.ontraport.app.parts.bb;
-
-import com.ontraport.app.tools.AbstractPart;
-
-public class MenuHelp extends AbstractPart
-{
-    public MenuHelp open () { return this; }
-    public MenuHelp close () { return this; }
-}
--- src/com/ontraport/app/parts/bb/MenuWorkflow.java
+++
@@ -1,9 +0,0 @@
-package com.ontraport.app.parts.bb;
-
-import com.ontraport.app.tools.AbstractPart;
-
-public class MenuWorkflow extends AbstractPart
-{
-    public MenuWorkflow open () { return this; }
-    public MenuWorkflow close () { return this; }
-}
