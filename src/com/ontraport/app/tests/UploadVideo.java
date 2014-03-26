6f6e0b3973df75ec6d28b55f34b27eb3ee52d200
Nick Cross <nick@ontraport.com>
2014-03-26 10:10:56 -0700

    changes for contact settings

--- src/com/ontraport/app/tests/UploadVideo.java
+++ src/com/ontraport/app/tests/UploadVideo.java
@@ -26,5 +26,6 @@ public class UploadVideo extends AbstractTest
         ZencoderVideo_ListAll zencoderVideo_ListAll = account_View.clickVideoManager();
         ZencoderVideo_Create zencoderVideo_Create = zencoderVideo_ListAll.clickNewVideo();
         zencoderVideo_Create.sendFile("/home/**/workspace/com.ontraport.app/etc/spacetestSMALL_512kb.mp4");
+        Thread.sleep(10000);
     }
 }
