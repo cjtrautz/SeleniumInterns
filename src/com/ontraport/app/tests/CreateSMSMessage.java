035a0fd14570d2c7dbfc99de4d18a740ad2bbc05
Mohan Embar <mohan@ontraport.com>
2014-05-07 22:23:02 -0400

    allow

--- src/com/ontraport/app/tests/CreateSMSMessage.java
+++ src/com/ontraport/app/tests/CreateSMSMessage.java
@@ -21,10 +21,10 @@ public class CreateSMSMessage extends AbstractTest
         Message_ListAll message_ListAll = contactListAll.menuPrimary.clickMessageListAll();
         Message_TypeSelection message_TypeSelection = message_ListAll.clickNewMessage();
         Message_CreateSMS message_CreateSMS = message_TypeSelection.clickSMSCreate();
-       // message_CreateSMS.openMergeFieldPane();
-       // message_CreateSMS.selectMergeField("First Name");
-       // message_CreateSMS.dialogBox.clickOk();
-        message_CreateSMS.enterSMSBody("Sel");
+        message_CreateSMS.openMergeFieldPane();
+        message_CreateSMS.selectMergeField("First Name");
+        message_CreateSMS.dialogBox.clickOk();
+       // message_CreateSMS.enterSMSBody("Sel");
         message_CreateSMS.enterMessageName("SelSMSMessage");
         message_ListAll = message_CreateSMS.clickSave();
         message_ListAll.formSearch.find("SelSMSMessage");
