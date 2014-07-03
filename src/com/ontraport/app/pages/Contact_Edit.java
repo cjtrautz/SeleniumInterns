2aca66ae59c6ba58ca4687d7bc6f984569c55474
Slayman <slayman@ontraport>
2014-07-03 16:43:34 -0700

    removed stupid import

--- src/com/ontraport/app/pages/Contact_Edit.java
+++ src/com/ontraport/app/pages/Contact_Edit.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import com.gargoylesoftware.htmlunit.javascript.host.Console;
 import com.ontraport.app.tools.AbstractPage;
 import com.ontraport.app.tools.AbstractPart;
 import com.ontraport.app.tools.AbstractSuite;
