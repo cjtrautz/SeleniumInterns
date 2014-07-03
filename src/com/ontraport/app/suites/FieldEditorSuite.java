8e7216341433d161eafaa482a95ada3b423b14aa
Nick Cross <nick@ontraport.com>
2014-07-02 17:31:27 -0700

    no more gmail

--- src/com/ontraport/app/suites/FieldEditorSuite.java
+++ src/com/ontraport/app/suites/FieldEditorSuite.java
@@ -291,8 +291,8 @@ public class FieldEditorSuite extends AbstractSuite
         ontraport_SignUp.enterState("c");
         ontraport_SignUp.enterZip(value.get("SignUp", "zip"));
         ontraport_SignUp.enterCountry("u");
-        ontraport_SignUp.enterEmailAddress("ontraportselenium+" + AbstractSuite.UNIQUE +"@gmail.com");
-        ontraport_SignUp.enterEmailAddressAgain("ontraportselenium+" + AbstractSuite.UNIQUE +"@gmail.com");
+        ontraport_SignUp.enterEmailAddress("ontraportselenium+" + AbstractSuite.UNIQUE +"@ontraport.com");
+        ontraport_SignUp.enterEmailAddressAgain("ontraportselenium+" + AbstractSuite.UNIQUE +"@ontraport.com");
         ontraport_SignUp.enterPassword("test123");
         ontraport_SignUp.enterPasswordAgain("test123");
         ontraport_SignUp.enterNameOnCard(value.get("SignUp", "name"));
